9071407388ec9671041ba349df3ddbb8d552918e
Vaughan Webb <vaughanw2012@gmail.com>
2019-12-21 15:49:05 +1300

    menu doen
    still broken

---
+++ Assets/Scripts/menu/bg.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bg : MonoBehaviour 
+{
+
+    public float r = 0;
+    public float g = 0;
+    public float b = 0;
+
+
+    void Update()
+    {
+        r += Time.deltaTime * 0.1f;
+        g += Time.deltaTime * 0.1f;
+        b += Time.deltaTime * 0.1f;
+
+        if (r >= 1.0f)
+        {
+            r = 0.0f; 
+        }
+        if (g >= 1.0f)
+        {
+           g = 0.0f;
+        }
+        if (b >= 1.0f)
+        {
+            b = 0.0f;
+        }
+
+        GetComponent<Image>().color = new Color(r, g, b, 0.5f);
+
+        //this.GetComponent<Camera>().clearFlags = CameraClearFlags.Color;
+        //this.GetComponent<Camera>().backgroundColor = new Color(r, g, b, 0.5f);
+    }
+}
---
+++ Assets/Scripts/menu/play.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class play : MonoBehaviour
+{
+    public void smashplay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void dontsmashplay()
+    {
+        Application.Quit();
+    }
+
+}
